0e973ba94615ad9af222e5fce5d1e69ff0620863
Charith <charithhewage@gmail.com>
2019-05-29 04:02:15 +1000

    integrations test file added

---
+++ spec/integration/toy_robot_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe 'Run Toy Robot with valid script' do
+	it 'should output 0,1,NORTH' do
+    
+  end
+end
+
+# describe 'Run Toy Robot with invalid script' do
+# end
