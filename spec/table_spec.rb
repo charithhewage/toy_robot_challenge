1d68a993ee1563db94d481bb5e2beb834d59e76e
Charith <charithhewage@gmail.com>
2019-05-29 01:49:28 +1000

    unit tests added for Table

---
+++ spec/table_spec.rb
@@ -0,0 +1,37 @@
+require "spec_helper"
+require "table"
+
+describe Table do
+
+  let(:table)   { Table.new(5,5) }
+
+  describe "#valid_position?" do
+    it "shold return true when x,y both are 0" do
+      expect(table.valid_position?(0, 0)).to be_truthy
+    end
+
+    it "should return false when x is less than 0" do
+      expect(table.valid_position?(-1, 0)).to be_falsy
+    end
+
+    it "should return false when y is less than 0" do
+      expect(table.valid_position?(0, -1)).to be_falsy
+    end
+
+    it "should return true when x and y are equal to table width and height" do
+      expect(table.valid_position?(5,5)).to be_truthy
+    end
+
+    it "should return false when x is greater than table width" do
+      expect(table.valid_position?(6, 0)).to be_falsy
+    end
+
+    it "should return false when y is greater than table height" do
+      expect(table.valid_position?(0, 6)).to be_falsy
+    end
+
+    it 'should return true when x,y are within the table width and height' do
+      expect(table.valid_position?(4, 3)).to be_truthy
+    end
+  end
+end
