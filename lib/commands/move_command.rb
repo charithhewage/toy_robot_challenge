010462524e450543566a4f6538041dfbf49e8bee
Charith <charithhewage@gmail.com>
2019-05-29 00:52:30 +1000

    table validations added to robot move,place commands

--- lib/commands/move_command.rb
+++ lib/commands/move_command.rb
@@ -1,5 +1,6 @@
 require_relative 'robot_command'
 require_relative '../directions/direction'
+require_relative '../error'
 
 module Commands
 	class MoveCommand < RobotCommand
@@ -8,7 +9,11 @@ module Commands
 			direction = Directions::Direction.new(robot.x, robot.y, robot.direction)
 			direction.move_forward
 			
-			robot.update_position(direction.x, direction.y, direction.direction)
+			if table.valid_position?(direction.x, direction.y)
+				robot.update_position(direction.x, direction.y, direction.direction)
+			else
+				raise Error.new("Invalid Position")
+			end
 			
     end
 	end
--- lib/commands/place_command.rb
+++ lib/commands/place_command.rb
@@ -1,4 +1,5 @@
 require_relative 'robot_command'
+require_relative '../error'
 
 module Commands
 	class PlaceCommand < RobotCommand
@@ -6,7 +7,7 @@ module Commands
 		def execute(robot, table)
 			x, y, direction = arguments
 
-			raise "Not Valid" unless table.valid_position?(x, y)
+			raise Error.new("Invalid Position") unless table.valid_position?(x, y)
 
       robot.update_position(x,y,direction)
     end
