e9b212017fcff18ca7505cd6a6e59a8bc614274a
Charith <charithhewage@gmail.com>
2019-05-29 10:32:22 +1000

    uncompleted tests commented

--- spec/integration/toy_robot_spec.rb
+++ spec/integration/toy_robot_spec.rb
@@ -1,10 +1,14 @@
 require 'spec_helper'
-
+=begin
 describe 'Run Toy Robot with valid script' do
 	it 'should output 0,1,NORTH' do
-    
+    file = File.expand_path('../../bin/sample_data_1.txt', File.dirname(__FILE__))
+
+
   end
 end
 
-# describe 'Run Toy Robot with invalid script' do
-# end
+describe 'Run Toy Robot with invalid script' do
+end
+
+=end
